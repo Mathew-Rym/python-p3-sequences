d98afbb44ac5be55d7305c2d838807b74d34c2b7
Mathew-Rym <rymmathew@gmail.com>
2025-05-18 23:40:03 +0300

    passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    a=0
+    b=1
+    result=[]
+    for i in range(length):
+        result.append(a)
+        c=a+b
+        a=b
+        b=c
+    print(result)
+        
+    
+print_fibonacci(9)
